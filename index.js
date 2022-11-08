f2dfa44d11befe50b29df07005dcb947cc702359
mohreza2019 <47902858+mohreza2019@users.noreply.github.com>
2022-11-08 19:13:13 +0000

    Create index.js

---
+++ index.js
@@ -0,0 +1,4 @@
+function replace () {
+    $('#lightbox > div:nth-child(3) > img').attr("src", "https://i.ibb.co/6RZsPZg/fax3.png");
+    $('#backgroundImage').css('background-image', 'url("https://i.ibb.co/Zgdq8pN/bg-fax.png")');
+}
